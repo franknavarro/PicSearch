6e85e08268cf62a00abc6cd523b773c330ae9a43
Frank Navarro <fnavarro7777@gmail.com>
2019-08-19 17:39:26 -0700

    Added CSS to tile images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import ImageCard from './ImageCard';
+import './ImageList.css';
 
-class ImageList extends React.Component {
-  render() {
-    const images = this.props.images.map(({ description, id, urls })  => {
-      return <img key={id} alt={description} src={urls.regular} />
-    });
-    return (
-      <div>
-        {images}
-      </div>
-    );
-  }
+const ImageList = (props) => {
+  const images = props.images.map((image)  => {
+    return <ImageCard key={image.id} image={image} />
+  });
+
+  return (
+    <div className="image-list">
+      {images}
+    </div>
+  );
 }
 
 export default ImageList;
